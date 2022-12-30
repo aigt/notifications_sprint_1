fe1bf9445f8a50e3683dbb720de1bcc0ce91deca
aigt <ai-kr@yandex.ru>
2022-12-30 19:12:29 +0500

    Добавлена структура файлов для воркера

---
+++ worker/src/main.py
@@ -0,0 +1,15 @@
+import logging
+from time import sleep
+from core import logger
+
+
+def main() -> None:
+    """Точка входа в приложение."""
+    logger.configure_logging()
+    while True:
+        logging.info("Hellow from worker")
+        sleep(15)
+
+
+if __name__ == "__main__":
+    main()
