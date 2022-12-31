74b9bb4a995c3335cd20ef445875571222bbbaab
wood <amper.sil.tok@gmail.com>
2022-12-31 13:35:56 +0300

    Добавлен мейн

---
+++ notification_api/src/main.py
@@ -0,0 +1,31 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+from core.config import get_settings
+from core.openapi_docs import API_DESCRIPTION, CONTACT
+
+settings = get_settings()
+
+app = FastAPI(
+    title=settings.project_name,
+    description=API_DESCRIPTION,
+    version=settings.api_version,
+    docs_url=settings.api_docs_url,
+    contact=CONTACT,
+    openapi_url=settings.openapi_url,
+    default_response_class=ORJSONResponse,
+)
+
+
+def local_start() -> None:
+    """Фунция для локального запуска приложения."""
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",  # noqa: S104
+        port=8000,  # noqa: WPS432
+    )
+
+
+if __name__ == "__main__":
+    local_start()
