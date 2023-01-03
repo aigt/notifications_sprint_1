a70db9880109c1a5e3c615a586ae8f0ceeb2606d
wood <amper.sil.tok@gmail.com>
2023-01-03 13:00:23 +0300

    Добавлены модели данных для уведомлений

---
+++ notification_api/src/models/model_for_queue.py
@@ -0,0 +1,22 @@
+from typing import Any, Literal, Optional
+
+from pydantic import EmailStr
+from services.orjson import OrjsonModel
+
+
+class Meta(OrjsonModel):
+    """Модель мета данных для модели Notification."""
+
+    urgency: Literal["immediate", "usual"]
+    scale: Literal["bulk", "individual"]
+    email: Optional[EmailStr]
+    periodic: bool
+
+
+class Notification(OrjsonModel):
+    """Модель уведомления для добавления в очередь."""
+
+    meta: Meta
+    type: Literal["show_subs", "info", "welcome"]
+    custom_template: Optional[Any]
+    fields: Optional[dict]
--- notification_api/src/models/welcome_models.py
+++ notification_api/src/models/welcome_models.py
@@ -1,9 +1,19 @@
-from pydantic import EmailStr
+from uuid import UUID
+
+from pydantic import EmailStr, Field
 from services.orjson import OrjsonModel
 
+from core.responses import NOTIFICATION_ADDED
 
-class NotifyRequest(OrjsonModel):
+
+class WelcomeNotifyRequest(OrjsonModel):
     """Модель запроса для отправки Welcome письма."""
 
-    user_id: str
+    user_id: UUID
     email: EmailStr
+
+
+class WelcomeNotifyResponse(OrjsonModel):
+    """Модель для ответа при запросе на отправку Welcome письма."""
+
+    message: str = Field(NOTIFICATION_ADDED)
