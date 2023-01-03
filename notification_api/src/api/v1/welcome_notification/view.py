48f3001f64e2225d4288a91bb169f62c7ac45dd4
wood <amper.sil.tok@gmail.com>
2023-01-03 10:49:41 +0300

    Добавлено представление для запроса на отправку Welcome уведомления

--- notification_api/src/api/v1/welcome_notification/view.py
+++ notification_api/src/api/v1/welcome_notification/view.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter
+import uuid
+
+from api.v1.welcome_notification.services import WelcomeService, get_welcome_service
+from fastapi import APIRouter, Body, Depends
+from models.welcome_models import WelcomeNotifyResponse
+from starlette import status
 
 from core.config import get_settings
 
@@ -6,9 +11,27 @@ welcome_router = APIRouter()
 settings = get_settings()
 
 
-@welcome_router.get(
+@welcome_router.post(
     path="/",
-    response_model="",
+    status_code=status.HTTP_200_OK,
+    response_model=WelcomeNotifyResponse,
 )
-async def welcome() -> None:
-    """Представление для отправления уведомления welcome."""
+async def welcome(
+    service: WelcomeService = Depends(get_welcome_service),
+    notify_data: dict = Body(
+        example={"user_id": str(uuid.uuid4()), "email": "example@gamil.com"},
+        title="Данные",
+        description="Идентификатор и почта пользователя для отправки уведомления",
+    ),
+) -> WelcomeNotifyResponse:
+    """Добавить закладку пользователю.
+
+    Args:
+        notify_data (dict): Данные пользователя которому будут отправлено Welcome уведомление
+        service (WelcomeService): Сервис. По умолчанию Depends(get_welcome_service).
+
+    Returns:
+        WelcomeNotifyResponse: Модель ответа.
+    """
+    await service.add_welcome_notification_in_queue(notify_data)
+    return WelcomeNotifyResponse()
