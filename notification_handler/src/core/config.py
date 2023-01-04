5ed0dae582ee252624ea6be504fc5349a2b84b6e
wood <amper.sil.tok@gmail.com>
2023-01-04 13:01:03 +0300

    Добавлены настройки postgres

--- notification_handler/src/core/config.py
+++ notification_handler/src/core/config.py
@@ -8,6 +8,13 @@ from pydantic import BaseSettings, Field
 class Settings(BaseSettings):
     """Настройки приложения."""
 
+    # Настройки Postgres
+    postgres_db: str = Field("notifications")
+    postgres_user: str = Field("app")
+    postgres_password: str = Field("pass")
+    postgres_host: str = Field("localhost")
+    postgres_port: int = Field(5432)
+
     # Настройки Rabbitmq
     rb_host: str = Field("localhost")
     rb_port: int = Field(5672)
