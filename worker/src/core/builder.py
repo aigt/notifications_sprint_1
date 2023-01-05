fe2b9fa78bc633b4633b79cd0f86bc38ed109efd
aigt <ai-kr@yandex.ru>
2023-01-05 15:42:21 +0500

    Добавлен файл сборки приложения

---
+++ worker/src/core/builder.py
@@ -0,0 +1,10 @@
+from worker_app import WorkerApp
+
+
+def build() -> WorkerApp:
+    """Собрать приложение.
+
+    Returns:
+        WorkerApp: Класс-приложение.
+    """
+    return NotImplemented
