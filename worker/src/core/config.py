3fe689ad8239750f3cc322050d1f8ea30d97b3d0
aigt <ai-kr@yandex.ru>
2023-01-05 20:27:31 +0500

    У воркера добавлены настройки RabbitMQ

--- worker/src/core/config.py
+++ worker/src/core/config.py
@@ -6,4 +6,15 @@ class Settings(BaseSettings):
 
     # Название проекта. Используется в Swagger-документации
     project_name: str = Field(default="Worker")
-    etl_version: str = "1.0.0"
+    app_version: str = "1.0.0"
+
+    # Настройки Rabbitmq
+    rb_host: str = Field("localhost")
+    rb_port: int = Field(5672)
+    rb_user: str = Field("user")
+    rb_password: str = Field("pass")
+    rb_max_tries_to_connect: int = Field(20)
+    rb_connect_retry_period: int = Field(
+        default=1,
+        description="Период повторных попыток подключения, с",
+    )
