ea722846158d35649cbb13f6b19555d70d7bc843
wood <amper.sil.tok@gmail.com>
2023-01-05 10:45:51 +0300

    Правки импортов

--- notification_handler/src/main.py
+++ notification_handler/src/main.py
@@ -1,16 +1,12 @@
-import logging
-
+from core.logger import configure_logging
+from core.settings import get_settings
 from core.start_up import start_up  # type: ignore
 from db.rabbit.callback import callback
 from db.rabbit.rabbitmq import get_rabbit
-
-from core.logger import configure_logging
-from core.settings import get_settings
 from core.add_queue import add_queue
 
 settings = get_settings()
 configure_logging()
-logger = logging.getLogger(__name__)
 
 
 def main() -> None:
