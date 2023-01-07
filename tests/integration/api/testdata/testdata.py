6eb5ce8400dca56cd047fd45961498d96379fa92
wood <amper.sil.tok@gmail.com>
2023-01-07 17:40:15 +0300

    Тесты актуализированы

--- tests/integration/api/testdata/testdata.py
+++ tests/integration/api/testdata/testdata.py
@@ -1,1 +1,11 @@
+import uuid
+
 EMAIL = "test_email@gmail.com"
+data_1 = {
+    "meta": {"urgency": "immediate", "scale": "bulk", "periodic": True},
+    "type": "show_subs",
+    "custom_template": "string",
+    "fields": {},
+    "user_id": str(uuid.uuid4()),
+    "email": EMAIL,
+}
