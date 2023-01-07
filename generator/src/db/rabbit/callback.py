b6ec22d0e199137c360b0b4a594a037ccc35dbe5
wood <amper.sil.tok@gmail.com>
2023-01-07 08:45:53 +0300

    Добавлена фабрика для mongo, добавлен генератор уведомлений в колбек функцию

--- generator/src/db/rabbit/callback.py
+++ generator/src/db/rabbit/callback.py
@@ -2,6 +2,10 @@ import orjson
 from pika.adapters.blocking_connection import BlockingChannel
 from pika.spec import Basic, BasicProperties
 
+from db.mongo.mongo import get_mongo
+from db.postgres.postgres import get_postgres
+from db.rabbit.rabbitmq import get_rabbit
+from generator.generator import Generator
 from models.notifications import Meta, NotificationFromNotifications
 
 
@@ -27,5 +31,12 @@ def callback(
         custom_template=notification.get("custom_template"),
         fields=notification.get("fields"),
     )
+
+    generator = Generator(
+        queue=get_rabbit(),
+        ugc_base=get_mongo(),
+        users_base=get_postgres(),
+    )
+    generator.create_data_for_worker(notification)
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
--- generator/src/generator/generator.py
+++ generator/src/generator/generator.py
@@ -1,2 +1,35 @@
+from db.base import BaseDatabase, BaseDocumentData, BaseQueue
+from models.notifications import NotificationFromNotifications, NotificationType
+
+
 class Generator:
     """Класс генерирующий уведомления для воркера получаемые из очереди."""
+
+    def __init__(
+        self,
+        queue: BaseQueue,
+        ugc_base: BaseDocumentData,
+        users_base: BaseDatabase,
+    ):
+        self.queue = queue
+        self.ugc_base = ugc_base
+        self.users_base = users_base
+
+    def create_data_for_worker(
+        self,
+        notification: NotificationFromNotifications,
+    ) -> None:
+        """Обработка уведомления для передачи в очередь воркера.
+
+        Args:
+            notification(NotificationFromNotifications): Уведомление из очереди.
+        """
+        if notification.type == NotificationType.welcome:
+            self.create_welcome(notification)
+
+    def create_welcome(self, notification: NotificationFromNotifications) -> None:
+        """Подготовка уведомления для welcome сообщения.
+
+        Args:
+            notification(NotificationFromNotifications): Уведомление
+        """
