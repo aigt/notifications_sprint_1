98b89b21f873adae516632f50ef26881690c9614
Kutaraev <artem.kutaraev@gmail.com>
2023-01-10 21:58:48 +0300

    Сообщения записываются в очеедь worker

--- scheduler/src/main.py
+++ scheduler/src/main.py
@@ -1,6 +1,17 @@
-from time import sleep
+import pika
 
-if __name__ == '__main__':
-    while True:
-        print("Container health check....")
-        sleep(3)
+
+def publish(channel, queue):
+    channel.basic_publish(exchange='',
+                          routing_key=queue,
+                          body='Hello World!')
+
+if __name__ == "__main__":
+    credentials = pika.PlainCredentials('user', 'pass')
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost',
+                                                                   port=5672,
+                                                                   credentials=credentials))
+
+    channel = connection.channel()
+
+    publish(channel, 'worker')
