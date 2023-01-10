8b1bc3a887e8a932055799a3db7019e71a75c32e
Kutaraev <artem.kutaraev@gmail.com>
2023-01-10 22:12:41 +0300

    Сообщения читаются из worker'a

--- scheduler/src/main.py
+++ scheduler/src/main.py
@@ -6,6 +6,16 @@ def publish(channel, queue):
                           routing_key=queue,
                           body='Hello World!')
 
+def callback(ch, method, properties, body):
+    print(f'Messagerecieved: {body}')
+
+def read(channel, queue, fn):
+    channel.basic_consume(queue=queue,
+                          auto_ack=True,
+                          on_message_callback=fn)
+    channel.start_consuming()
+
+
 if __name__ == "__main__":
     credentials = pika.PlainCredentials('user', 'pass')
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost',
@@ -15,3 +25,5 @@ if __name__ == "__main__":
     channel = connection.channel()
 
     publish(channel, 'worker')
+    read(channel, 'worker', callback)
+
