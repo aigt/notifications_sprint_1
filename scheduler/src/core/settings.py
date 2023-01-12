c6f8e21bdc2c42eae90eb18f07b347d4e3640e68
Kutaraev <artem.kutaraev@gmail.com>
2023-01-12 21:32:51 +0300

    доработана структура папок

---
+++ scheduler/src/core/settings.py
@@ -0,0 +1,24 @@
+from functools import lru_cache
+
+from pydantic import BaseSettings, Field
+
+
+class Settings(BaseSettings):
+    """Настройки приложения."""
+    # Настройки Rabbitmq
+    rb_host: str = Field("localhost")
+    rb_port: int = Field(5672)
+    rb_user: str = Field("user")
+    rb_password: str = Field("pass")
+    rb_receiving_queue: str = Field("generator")
+    rb_transfer_queue: str = Field("generator")
+    rb_exchange: str = Field("notifications")
+
+@lru_cache()
+def get_settings() -> Settings:
+    """Фабрика настроек.
+
+    Returns:
+        Settings: Класс с настройками приложения.
+    """
+    return Settings()
