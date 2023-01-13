2de4b2baa2c6dbb1424c0fa273f128ab50af15ee
aigt <ai-kr@yandex.ru>
2023-01-13 22:08:46 +0500

    Рефакторинг воркера

--- worker/src/workers/email.py
+++ worker/src/workers/email.py
@@ -1,55 +1,34 @@
-import logging
+from typing import Optional
 
 from errors.exceptions import NoEmailForEmailWorkerError
-from services.email_publisher import EmailPublisher
-from services.render import Render
 from workers.worker import Worker, WorkerMessage
 
 
 class EmailWorker(Worker):
     """Воркер обрабатывающий Email."""
 
-    templates_target_name = "email"
+    @property
+    def templates_target_name(self) -> str:
+        """Целевой способ доставки сообщений.
 
-    def __init__(
-        self,
-        email_publisher: EmailPublisher,
-        email_render: Render,
-    ) -> None:
-        self._email_publisher = email_publisher
-        self._email_render = email_render
+        Returns:
+            str: Способ.
+        """
+        return "email"
 
-    def run(self, message: WorkerMessage) -> None:
-        """Обработать сообщение.
+    def get_client_id_from_message(self, message: WorkerMessage) -> str:
+        """Получить email клиента для публикации.
 
         Args:
-            message (WorkerMessage): Сообщение для обработки.
+            message (WorkerMessage): Сообщение, из которого извлечь email.
+
+        Returns:
+            str: Email.
 
         Raises:
-            NoEmailForEmailWorkerError: Если не указан email
+            NoEmailForEmailWorkerError: Если Email не указан.
         """
-        if not message.email:
+        email: Optional[str] = message.email
+        if email is None:
             raise NoEmailForEmailWorkerError()
-
-        logging.info(
-            "Worker start to process message: %s",  # noqa: WPS323
-            message,
-        )
-
-        rendered_email = self._email_render(
-            template=message.template,
-            target=self.templates_target_name,
-            fields=message.fields,
-        )
-
-        logging.info(
-            "Worker rendered email: %s",  # noqa: WPS323
-            rendered_email,
-        )
-
-        self._email_publisher.publish(
-            email=message.email,
-            email_content=rendered_email,
-        )
-
-        logging.info("Worker published email")
+        return email
