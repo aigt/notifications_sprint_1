46c715be0734c591ca902e51ae36d39943980350
aigt <ai-kr@yandex.ru>
2023-01-13 22:20:09 +0500

    Реализован воркер сохранения истории

--- worker/src/workers/history.py
+++ worker/src/workers/history.py
@@ -1,13 +1,34 @@
+from typing import Optional
+
+from errors.exceptions import NoUserIdForHistoryWorkerError
 from workers.worker import Worker, WorkerMessage
 
 
 class HistoryWorker(Worker):
     """Воркер заносящий оповещения в историю."""
 
-    def run(self, message: WorkerMessage) -> None:
-        """Обработать сообщение.
+    @property
+    def templates_target_name(self) -> str:
+        """Целевой способ доставки сообщений.
+
+        Returns:
+            str: Способ.
+        """
+        return "history"
+
+    def get_client_id_from_message(self, message: WorkerMessage) -> str:
+        """Получить идентификатор клиента для публикации.
 
         Args:
-            message (WorkerMessage): Сообщение для обработки.
+            message (WorkerMessage): Сообщение, из которого извлечь идентификатор.
+
+        Returns:
+            str: Идентификатор.
+
+        Raises:
+            NoUserIdForHistoryWorkerError: Если не указан user_id.
         """
-        return  # noqa: WPS324
+        user_id: Optional[str] = message.fields.get("user_id", None)
+        if user_id is None:
+            raise NoUserIdForHistoryWorkerError()
+        return user_id
