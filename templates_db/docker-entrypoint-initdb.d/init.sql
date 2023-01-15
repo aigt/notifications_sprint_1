2be4d2d9c57722a9ee8327aba54fb38f1700f18d
aigt <ai-kr@yandex.ru>
2023-01-15 14:57:37 +0500

    Добавлена инициализация новых типов шаблонов

--- templates_db/docker-entrypoint-initdb.d/init.sql
+++ templates_db/docker-entrypoint-initdb.d/init.sql
@@ -24,6 +24,24 @@ CREATE TABLE IF NOT EXISTS notify_templates.history (
     , UNIQUE (name)
 );
 
+CREATE TABLE IF NOT EXISTS notify_templates.sms (
+      id          uuid PRIMARY KEY DEFAULT uuid_generate_v4()
+    , name        VARCHAR(128) NOT NULL
+    , template    TEXT NOT NULL
+    , created_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    , modified_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    , UNIQUE (name)
+);
+
+CREATE TABLE IF NOT EXISTS notify_templates.push (
+      id          uuid PRIMARY KEY DEFAULT uuid_generate_v4()
+    , name        VARCHAR(128) NOT NULL
+    , template    TEXT NOT NULL
+    , created_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    , modified_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    , UNIQUE (name)
+);
+
 
 
 INSERT INTO notify_templates.email (
@@ -70,6 +88,64 @@ VALUES
     );
 
 INSERT INTO notify_templates.history (
+      name
+    , template
+)
+VALUES
+    (
+          'welcome'
+
+        , 'Добро пожаловать!\n'
+          '\n'
+          'Привет {{ name }}!\n'
+          'Рады приветствовать тебя в нашем кинотеатре!'
+    ),
+    (
+          'info'
+
+        , '{{ title }}\n'
+          '\n'
+          '{{ text }}'
+    ),
+    (
+          'show_subs'
+
+        , '{{ title }}\n'
+          '{{ movie }}\n'
+          '\n'
+          '{{ text }}'
+    );
+
+INSERT INTO notify_templates.sms (
+      name
+    , template
+)
+VALUES
+    (
+          'welcome'
+
+        , 'Добро пожаловать!\n'
+          '\n'
+          'Привет {{ name }}!\n'
+          'Рады приветствовать тебя в нашем кинотеатре!'
+    ),
+    (
+          'info'
+
+        , '{{ title }}\n'
+          '\n'
+          '{{ text }}'
+    ),
+    (
+          'show_subs'
+
+        , '{{ title }}\n'
+          '{{ movie }}\n'
+          '\n'
+          '{{ text }}'
+    );
+
+INSERT INTO notify_templates.push (
       name
     , template
 )
